d244e1f01ec8b25603fc149cc470520449b39d93
TARUNKY18 <kunduryogesh@gmail.com>
2024-07-06 22:29:20 +0530

    hashing user password and authorizing the routes using jwt also adding login-logout endpoints

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,5 +1,6 @@
 import { User } from "../models/user.model.js" 
 import bcrypt from "bcryptjs"
+import generateTokenAndSetCookies from "../utils/generateToken.js"
 
 const signupUser = async (req, res) => {
     try {
@@ -34,6 +35,8 @@ const signupUser = async (req, res) => {
         })
 
         if(newUser){
+
+            generateTokenAndSetCookies(newUser._id, res)
             await newUser.save()
 
         res.status(201).json({
@@ -54,11 +57,49 @@ const signupUser = async (req, res) => {
 }
 
 const loginUser = async (req, res) => {
-    res.send("User login")
+    try {
+        
+        const { userName, password } = req.body
+        
+        if(!userName ||!password) {
+            return res.status(400).json({ message: "All fields are required" })
+        }
+
+        const user = await User.findOne({ userName })
+        const isPasswordCorrect = bcrypt.compare(password, user?.password || "")
+
+        if(!user || !isPasswordCorrect) {
+            return res.status(401).json({ message: "Invalid credentials" })
+        }
+
+        generateTokenAndSetCookies(user._id, res)
+
+        res.status(200).json({
+            _id : user._id,
+            userName: user.userName,
+            fullName: user.fullName,
+            profilePicture: user.profilePicture
+        })
+
+    } catch (error) {
+        console.error("Error in login", error.message)
+        res.status(500).json({ message: "Internal Server Error" })
+    }
 }
 
 const logoutUser = async (req, res) => {
-    res.send("User logout")
+    try {
+
+        res.cookie("jwt", "", { maxAge: 0 })
+        
+        res.status(200).json({
+            message: "Logged out successfully"  
+        })
+
+    } catch (error) {
+        console.error("Error in login", error.message)
+        res.status(500).json({ message: "Internal Server Error" })
+    }
 }
 
 export {
